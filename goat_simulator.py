06cbf1a0c6ef56963af3be597d49689b88c1aa9f
nooblina <schleichangelina@gmail.com>
2026-06-12 02:54:57 +0200

    help

---
+++ goat_simulator.py
@@ -0,0 +1,34 @@
+ 
+# Textbasierter Goat-Simulator in Python
+
+""" Feature:
+	Map
+	Secrets
+	Quests
+	Fähigkeiten
+	Chaos-Level -> XP
+	Achievements
+	Collectables
+	Freischaltbare Ziegen -> versch. Eigenschaften
+	Tragbare Items mit Effekten
+"""
+
+
+
+# Map-Struktur
+
+MAP = """
+						Weltname
+							|
+						 Zuhause
+							|
+	 Einkaufszentrum -- Marktplatz -- Bahnhof   
+	 		|				|		   /	\
+ 	 Skin/Item-Store      Park  --  Farm -- Schwarzmarkt 
+	 	/   \		     /	  \
+   Imbiss  Kino  	Museum   Schwimmbad
+								  |
+							Kanalisation						
+"""
+
+
