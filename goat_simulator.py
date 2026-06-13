373169b4363b4811123fe228fd4929babb03d45b
nooblina <schleichangelina@gmail.com>
2026-06-13 03:55:59 +0200

    world data

--- goat_simulator.py
+++ goat_simulator.py
@@ -1,7 +1,14 @@
- 
-# Textbasierter Goat-Simulator in Python
+# =======================================================================================
+# Textbased Goat-Simulator in Python
+# =======================================================================================
 
-""" Feature:
+
+# ---------------------------------------------------------------------------------------
+# Feature ideas
+# ---------------------------------------------------------------------------------------
+
+
+"""
 	Map
 	Secrets
 	Quests
@@ -14,8 +21,10 @@
 """
 
 
+# ----------------------------------------------------------------------------------------
+# World map
+# ----------------------------------------------------------------------------------------
 
-# Map-Struktur
 
 MAP = """
 						Weltname
@@ -23,12 +32,107 @@ MAP = """
 						 Zuhause
 							|
 	 Einkaufszentrum -- Marktplatz -- Bahnhof   
-	 		|				|		   /	\
- 	 Skin/Item-Store      Park  --  Farm -- Schwarzmarkt 
-	 	/   \		     /	  \
-   Imbiss  Kino  	Museum   Schwimmbad
-								  |
-							Kanalisation						
+	 	/		\			|		   /	\
+Erdgeschoss -- 1. Stock    Park  --  Farm -- Schwarzmarkt 
+	 |			  |		   /	\
+ Boutiquen		  |	  Museum   Schwimmbad
+	 		   /	\			 	|
+			Imbiss  Kino	  Kanalisation						
 """
 
+# ----------------------------------------------------------------------------------------
+# World data
+# ----------------------------------------------------------------------------------------
+
+
+places = {
+ 
+    "Zuhause": {
+		"name": "Zuhause",
+    	"connections": ["Marktplatz"]
+	},
+
+	"Marktplatz": {
+        "name": "Marktplatz",
+        "connections": ["Zuhause", "Einkaufszentrum", "Bahnhof", "Park"]
+	},
+    
+	"Einkaufszentrum": {
+        "name": "Einkaufszentrum",
+        "connections": ["Marktplatz", "Erdgeschoss", "1. Stock"]
+	},
+    
+	"Bahnhof": {
+        "name": "Bahnhof",
+        "connections": ["Marktplatz", "Farm", "Schwarzmarkt"]
+	},
+    
+	"Erdgeschoss": {
+        "name": "Erdgeschoss",
+        "connections": ["Einkaufszentrum", "1. Stock", "Boutiquen"]
+	},
+    
+	"1. Stock": {
+        "name": "1. Stock",
+        "connections": ["Einkaufszentrum", "Erdgeschoss", "Imbiss", "Kino"]
+	},
+    
+	"Park": {
+        "name": "Park",
+        "connections": ["Marktplatz", "Museum", "Schwimmbad"]
+	},
+    
+	"Farm": {
+        "name": "Farm",
+        "connections": ["Bahnhof", "Park", "Schwarzmarkt"]
+	},
+    
+	"Schwarzmarkt": {
+        "name": "Schwarzmarkt",
+		"connections": ["Bahnhof", "Farm"]
+	},
+    
+	"Boutiquen": {
+        "name": "Boutiquen",
+        "connections": ["Erdgeschoss"]
+	},
+    
+	"Museum": {
+        "name": "Museum",
+        "connections": ["Park"]
+	},
+    
+	"Schwimmbad": {
+		"name": "Schwimmbad",
+        "connections": ["Park", "Kanalisation"]
+	},
+    
+	"Imbiss": {
+        "name": "Imbiss",
+        "connections": ["1. Stock"]
+	},
+    
+	"Kino": {
+        "name": "Kino",
+        "connections": ["1. Stock"]
+	},
+    
+	"Kanalisation": {
+        "name": "Kanalisation",
+        "connections": ["Schwimmbad"]
+	}
+} 
+
+
+# ----------------------------------------------------------------------------------------
+# Player data
+# ----------------------------------------------------------------------------------------
+
+current_place = "Zuhause"
+inventory = []
+chaos = 0
+
+print("Standort:", current_place)
+print("Chaos-Level:", chaos)
+print("Inventar:", inventory)
 
